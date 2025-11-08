af843ed91d027cf9a2893b4949cfcc0bbc7bfd65
Jasmin Lauper <jasmin.lauper@students.bfh.ch>
2025-11-08 21:45:59 +0100

    Hamburger-Menü hinzugefügt, CSS-Variablen implementiert und Timeline-Styles entfernt

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
 var typed = new Typed("#typed", {
-  strings: ["Hallo, ich bin Jasmin", "Business Analystin."],
+  strings: [
+    "ich bin jasmin, schadenspezialistin.",
+    "ich bin jasmin, business analystin.",
+  ],
   typeSpeed: 50,
-  backSpeed: 25,
-  loop: true,
+  backSpeed: 30,
+  backDelay: 1000,
+  startDelay: 500,
+  loop: false,
+  smartBackspace: true,
 });
